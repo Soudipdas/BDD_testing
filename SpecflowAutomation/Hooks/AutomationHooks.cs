43ff64d3a28bcd083791d667b0ba92b6bb659820
Soudipdas <48681987+Soudipdas@users.noreply.github.com>
2022-09-13 15:13:04 +0530

    Update AutomationHooks.cs

--- SpecflowAutomation/Hooks/AutomationHooks.cs
+++ SpecflowAutomation/Hooks/AutomationHooks.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
 namespace SpecflowAutomation.Hooks
 {
     [Binding]
